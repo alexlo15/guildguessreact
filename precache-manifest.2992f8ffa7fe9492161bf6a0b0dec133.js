afb443e1ef4c1c6b9abb3980a433e384e3e45ed4
Alex Lo <loalexander389@gmail.com>
2019-08-27 17:54:36 -0400

    Updates

---
+++ precache-manifest.2992f8ffa7fe9492161bf6a0b0dec133.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159b2a578550fef0e786ac19f50dadbe",
+    "url": "/guildguessreact/index.html"
+  },
+  {
+    "revision": "963a68c240abff5a95a2",
+    "url": "/guildguessreact/static/css/main.039b8c1d.chunk.css"
+  },
+  {
+    "revision": "4339dd79dddf86936ee9",
+    "url": "/guildguessreact/static/js/2.c07d1b80.chunk.js"
+  },
+  {
+    "revision": "963a68c240abff5a95a2",
+    "url": "/guildguessreact/static/js/main.af9d6504.chunk.js"
+  },
+  {
+    "revision": "db46b2959066a5f6a61c",
+    "url": "/guildguessreact/static/js/runtime~main.b51dbfed.js"
+  },
+  {
+    "revision": "bbb235d45da20f882c4b641dabc2e15d",
+    "url": "/guildguessreact/static/media/mtg.bbb235d4.jpg"
+  }
+]);
